06fe0190f6aba04e1b2f313dca038e5d36e4fa48
MatanNahum <matan.nahum95@gmail.com>
2022-09-05 14:03:44 +0300

    check

--- Random User Page API Project/src/Model.ts
+++ Random User Page API Project/src/Model.ts
@@ -1,1 +1,22 @@
 console.log("Model");
+
+class Model{
+    const personData= $.get( "https://randomuser.me/api/")
+    
+    constructor(personData){
+        this.personData = personData
+
+    }
+}
+
+// $.ajax({
+//   url: "https://randomuser.me/api/",
+//   dataType: "json",
+//   success: function (data) {
+//     console.log(data);
+//   },
+// });
+
+$.get("https://randomuser.me/api/").then{data => console.log(data)
+
+}
