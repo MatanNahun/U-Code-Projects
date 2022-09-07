9eb5d686aff6a1d96fae7785f5e736cfba3e17c1
MatanNahum <matan.nahum95@gmail.com>
2022-09-07 12:32:07 +0300

    render done

--- Random User Page API Project/src/Controller.ts
+++ Random User Page API Project/src/Controller.ts
@@ -7,3 +7,4 @@ model3.getAllData().then((res) => render.renderUser(model3.user));
 model3.getAllData().then((res) => render.renderFriends(model3.user.friends));
 model3.getAllData().then((res) => render.renderQuote(model3.quoteData.text));
 model3.getAllData().then((res) => render.renderPokemon(model3.pokemonData));
+model3.getAllData().then((res) => render.renderAbout(model3.about));
