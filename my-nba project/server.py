5d779127e5edec9a121b21e31c17c6c4d47f7414
MatanNahum <matan.nahum95@gmail.com>
2022-09-20 16:01:58 +0300

    show_players_by_teamName_and_year

--- my-nba project/server.py
+++ my-nba project/server.py
@@ -1,1 +1,51 @@
-"server"
+from tkinter.messagebox import YESNOCANCEL
+from webbrowser import get
+from fastapi import FastAPI
+from fastapi import Request, Response
+import requests
+import uvicorn
+
+app = FastAPI()
+
+
+teams_id = {
+    "lakers": "1610612747",
+    "warriors": "1610612744",
+    "heat": "1610612748",
+    "suns": "1610612756",
+}
+
+
+@app.get("/")
+def root():
+    return {"message": "Server is up and running"}
+
+
+@app.get("/sanity")
+def sanity():
+    return {"message": "Server is up and running in sanity"}
+
+
+@app.get("/teams")
+async def show_teams():
+    teams = requests.get("http://data.nba.net/data/10s/prod/v1/2020/teams.json")
+    return teams.json()
+
+
+def get_player_by_year(year):
+    players = requests.get(f"http://data.nba.net/10s/prod/v1/{year}/players.json")
+    return players.json()["league"]["standard"]
+
+
+@app.get("/players/{teamName}/{year}")
+async def show_players_by_teamName_and_year(teamName, year):
+    players_relevant_data = get_player_by_year(year)
+    return [
+        player
+        for player in players_relevant_data
+        if player["teamId"] == teams_id[teamName]
+    ]
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
