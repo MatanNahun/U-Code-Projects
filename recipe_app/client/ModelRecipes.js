9788b9f765f23a4dab5d1aabf0681381ab77d6cd
MatanNahum <matan.nahum95@gmail.com>
2022-10-23 14:44:30 +0300

    gluten free filter done

--- recipe_app/client/ModelRecipes.js
+++ recipe_app/client/ModelRecipes.js
@@ -6,6 +6,13 @@ class ModelRecipes {
 
   async getReciepesData(ingredient) {
     let URL = `/recipes/${ingredient}`;
+    const checkbox_input = document.getElementById("checkbox");
+    if (checkbox_input.checked == true) {
+      console.log("checkbox true");
+      URL = `/recipes/${ingredient}?diet=gluten_free`;
+    }
+
+    // let URL = `/recipes/${ingredient}`;
 
     let reciepesData = await $.get(URL);
     console.log(reciepesData);
--- recipe_app/server.py
+++ recipe_app/server.py
@@ -22,6 +22,33 @@ def sanity():
     return {"message": "Server is up and running in sanity"}
 
 
+# @app.get("/recipes/{ingredient}")
+# def get_recipes_by_ingredient(ingredient, diet=None):
+
+#     recipes_by_ingredient_raw = requests.get(
+#         f"https://recipes-goodness.herokuapp.com/recipes/{ingredient}"
+#     )
+#     recipes_by_ingredient = recipes_by_ingredient_raw.json()["results"]
+
+#     if diet == "gluten_free":
+#         gluten_ingredients = get_gluten_ingredients()
+
+#         recipes_gluten_free = []
+#         for reciepe in recipes_by_ingredient:
+#             for ingredient in reciepe["ingredients"]:
+#                 print(ingredient)
+#                 if ingredient in gluten_ingredients:
+#                     print("true")
+#                     break
+#                 else:
+#                     recipes_gluten_free.append(reciepe)
+#             return recipes_gluten_free
+
+#         return recipes_gluten_free
+
+#     return recipes_by_ingredient
+
+
 @app.get("/recipes/{ingredient}")
 def get_recipes_by_ingredient(ingredient, diet=None):
 
@@ -31,27 +58,24 @@ def get_recipes_by_ingredient(ingredient, diet=None):
     recipes_by_ingredient = recipes_by_ingredient_raw.json()["results"]
 
     if diet == "gluten_free":
-        gluten_ingredients = get_gluten_ingredients()
-
         recipes_gluten_free = []
         for reciepe in recipes_by_ingredient:
-            for ingredient in reciepe["ingredients"]:
-                print(ingredient)
-                if ingredient in gluten_ingredients:
-                    print("true")
-                    break
-                else:
-                    recipes_gluten_free.append(reciepe)
-            return recipes_gluten_free
+            if is_gluten_free(reciepe):
+                recipes_gluten_free.append(reciepe)
 
         return recipes_gluten_free
 
     return recipes_by_ingredient
 
 
-def is_gluten_free():
-    gluten_ingredient = get_gluten_ingredients()
-    return
+def is_gluten_free(reciepe):
+    gluten_ingredients = get_gluten_ingredients()
+
+    for ingredient in reciepe["ingredients"]:
+        if ingredient in gluten_ingredients:
+            print(reciepe)
+            return False
+    return True
 
 
 if __name__ == "__main__":
