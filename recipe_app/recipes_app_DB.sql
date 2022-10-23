d4f0f0a15bbb928053b4e373bc44f403ce10efb8
MatanNahum <matan.nahum95@gmail.com>
2022-10-23 09:59:38 +0300

    diet data-base done

---
+++ recipe_app/recipes_app_DB.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE recipes_app;
+
+USE recipes_app;
+
+-- CREATE TABLE dairy_ingredients(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255)
+-- );
+
+-- CREATE TABLE gluten_ingredients(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255)
+-- );
+
+-- INSERT INTO dairy_ingredients VALUES(1 , "Cream");
+-- INSERT INTO dairy_ingredients VALUES(2 , "Cheese");
+-- INSERT INTO dairy_ingredients VALUES(3 , "Milk");
+-- INSERT INTO dairy_ingredients VALUES(4 , "Butter");
+-- INSERT INTO dairy_ingredients VALUES(5 , "Creme");
+-- INSERT INTO dairy_ingredients VALUES(6 , "Ricotta");
+-- INSERT INTO dairy_ingredients VALUES(7 , "Mozzarella");
+-- INSERT INTO dairy_ingredients VALUES(8 , "Custard");
+-- INSERT INTO dairy_ingredients VALUES(9 , "Cream Cheese");
+
+-- INSERT INTO gluten_ingredients VALUES(1 , "Flour");
+-- INSERT INTO gluten_ingredients VALUES(2 , "Bread");
+-- INSERT INTO gluten_ingredients VALUES(3 , "spaghetti");
+-- INSERT INTO gluten_ingredients VALUES(4 , "Biscuits");
+-- INSERT INTO gluten_ingredients VALUES(5 , "Beer");
+
+
+
+
+
+
+SELECT * FROM gluten_ingredients
--- recipe_app/server.py
+++ recipe_app/server.py
@@ -16,5 +16,18 @@ def root():
     return FileResponse("index.html")
 
 
+@app.get("/sanity")
+def sanity():
+    return {"message": "Server is up and running in sanity"}
+
+
+@app.get("/recipes")
+def get_recipes_by_ingredient(ingredient):
+    recipes_by_ingredient = requests.get(
+        f"https://recipes-goodness.herokuapp.com/recipes/{ingredient}"
+    )
+    return recipes_by_ingredient.json()
+
+
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8080, reload=True)
