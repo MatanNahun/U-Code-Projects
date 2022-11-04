42c44f58deb2188004010822849443011c3cad7e
MatanNahum <matan.nahum95@gmail.com>
2022-11-04 11:27:50 +0200

    hangman- part 1

---
+++ Hangman/hangman-project/src/components/Solution.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Solution extends Component {
+  render() {
+    return (
+      <div>
+        <span>_ </span>
+        <span>_ </span>
+        <span>_ </span>
+        <span>_ </span>
+        <span>_ </span>
+        <div>your ideal mood when coding.</div>
+      </div>
+    );
+  }
+}
+
+export default Solution;
