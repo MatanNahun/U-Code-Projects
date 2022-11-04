fa036bfbdd35615a3842bdf974df6c99e560a3d4
MatanNahum <matan.nahum95@gmail.com>
2022-11-04 12:06:21 +0200

    letterStatus function

--- Hangman/hangman-project/src/App.js
+++ Hangman/hangman-project/src/App.js
@@ -5,9 +5,20 @@ import Solution from "./components/Solution";
 import Letters from "./components/Letters";
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.state = {
+      letterStatus: this.generateLetterStatuses(),
+    };
+  }
+
+  generateLetterStatuses() {
+    let letterStatus = {};
+    for (let i = 65; i < 91; i++) {
+      letterStatus[String.fromCharCode(i)] = false;
+    }
+    return letterStatus;
+  }
 
   render() {
     return (
