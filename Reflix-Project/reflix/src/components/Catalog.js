1f8a6f0258b3edc12194cde8370a8d5361ef0386
MatanNahum <matan.nahum95@gmail.com>
2022-11-11 16:43:32 +0200

    seperate catalog into functions

--- Reflix-Project/reflix/src/components/Catalog.js
+++ Reflix-Project/reflix/src/components/Catalog.js
@@ -6,6 +6,47 @@ import "../styles/Catalog.css";
 import Search from "./Search";
 
 class Catalog extends Component {
+  renderCatalogMovies = () => {
+    const movies = this.props.movies;
+    const filterSearchTerm = this.props.filterSearchTerm;
+    return movies
+      .filter((movie) =>
+        movie.title.toLowerCase().includes(filterSearchTerm.toLowerCase())
+      )
+      .map((movie) => {
+        return (
+          <Movie
+            className="movie"
+            key={movie.id}
+            movie={movie}
+            rentMovie={this.props.rentMovie}
+            removeMovie={this.props.removeMovie}
+          ></Movie>
+        );
+      });
+  };
+
+  renderRentedMovies = () => {
+    const movies = this.props.movies;
+    const filterSearchTerm = this.props.filterSearchTerm;
+    return movies
+      .filter(
+        (movie) =>
+          movie.isRented === true &&
+          movie.title.toLowerCase().includes(filterSearchTerm.toLowerCase())
+      )
+      .map((movie) => {
+        return (
+          <Movie
+            className="movie"
+            key={movie.id}
+            movie={movie}
+            removeMovie={this.props.removeMovie}
+          ></Movie>
+        );
+      });
+  };
+
   render() {
     const movies = this.props.movies;
     const filterSearchTerm = this.props.filterSearchTerm;
@@ -18,44 +59,9 @@ class Catalog extends Component {
         ></Search>
         <div>Budget: ${this.props.budget}</div>
         <h1 id="movies-rented-title">rented:</h1>
-        <div id="movies-rented-container">
-          {movies
-            .filter(
-              (movie) =>
-                movie.isRented === true &&
-                movie.title
-                  .toLowerCase()
-                  .includes(filterSearchTerm.toLowerCase())
-            )
-            .map((movie) => {
-              return (
-                <Movie
-                  className="movie"
-                  key={movie.id}
-                  movie={movie}
-                  removeMovie={this.props.removeMovie}
-                ></Movie>
-              );
-            })}
-        </div>
+        <div id="movies-rented-container">{this.renderRentedMovies()}</div>
         <h1 id="movies-catalog-title">Catalog:</h1>
-        <div id="movies-catalog-container">
-          {movies
-            .filter((movie) =>
-              movie.title.toLowerCase().includes(filterSearchTerm.toLowerCase())
-            )
-            .map((movie) => {
-              return (
-                <Movie
-                  className="movie"
-                  key={movie.id}
-                  movie={movie}
-                  rentMovie={this.props.rentMovie}
-                  removeMovie={this.props.removeMovie}
-                ></Movie>
-              );
-            })}
-        </div>
+        <div id="movies-catalog-container">{this.renderCatalogMovies()}</div>
       </div>
     );
   }
